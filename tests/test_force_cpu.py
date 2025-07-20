4d6491963d12e6243e5fb4a9b4dab926bedde297
averinaleks <averinaleks92@yandex.ru>
2025-07-20 15:58:07 +0300

    Fix utils reload in force_cpu test

--- tests/test_force_cpu.py
+++ tests/test_force_cpu.py
@@ -1,9 +1,11 @@
 import os
 import importlib
+import sys
 import utils
 
 
-def test_force_cpu():
-    os.environ["FORCE_CPU"] = "1"
+def test_force_cpu(monkeypatch):
+    monkeypatch.setenv("FORCE_CPU", "1")
+    sys.modules["utils"] = utils
     importlib.reload(utils)
     assert utils.is_cuda_available() is False
