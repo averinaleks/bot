5b0a4b36268a1526d03b4aa560d11ae8e5176942
averinaleks <averinaleks92@yandex.ru>
2025-08-24 14:10:37 +0300

    Use localhost as default host

--- server.py
+++ server.py
@@ -230,5 +230,5 @@ if __name__ == "__main__":
     import uvicorn
 
     port = int(os.getenv("PORT", "8000"))
-    host = os.getenv("HOST", "0.0.0.0")
+    host = os.getenv("HOST", "127.0.0.1")
     uvicorn.run("server:app", host=host, port=port)
