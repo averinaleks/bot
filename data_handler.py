babed53fcc424755559582229987c59b1274edb9
averinaleks <averinaleks92@yandex.ru>
2025-08-26 20:46:26 +0300

    Explicitly configure logging in entry points

--- data_handler.py
+++ data_handler.py
@@ -2926,7 +2926,10 @@ def ping():
 
 
 if __name__ == "__main__":
+    from bot.utils import configure_logging
+
     load_dotenv()
+    configure_logging()
     host = os.getenv("HOST", "127.0.0.1")
     port = int(os.getenv("PORT", "8000"))
     logger.info("Запуск сервиса DataHandler на %s:%s", host, port)
